e6430934ad52295a32c9740a86f774161c099989
marcosf <marcosflaviosilveira@gmail.com>
2016-09-21 19:34:04 -0300

    as

--- src/br/ufc/data/mining/main/DBScan.java
+++ src/br/ufc/data/mining/main/DBScan.java
@@ -1,7 +1,6 @@
 package br.ufc.data.mining.main;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -22,24 +21,15 @@ public class DBScan {
 	 * processamento Esses resultados serao as regioes As regioes podem ser um
 	 * list de sets
 	 */
-	@SuppressWarnings({ "rawtypes" })
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static void main(String[] args) {
 		DayDAO dao = new DayDAO();
 		String[] days = { "segunda", "terca", "quarta", "quinta", "sexta" };
 		Class[] classes = { MonDrive.class, TueDrive.class, WedDrive.class, ThuDrive.class, FriDrive.class };
-		// Dataset completo
 		List<DayDrive> dataSet = new ArrayList<>();
-		// Clusters separados por dias
-		HashMap<String, List<Cluster>> dayClusters = new HashMap<String, List<Cluster>>();
-		
 		for (int i = 0; i < 5; i++) {
-<<<<<<< HEAD
 			dataSet = (List<DayDrive>) (List<?>) dao.getAllByDayAndHour(days[i], "13:00:00", "14:00:00", classes[i]);
-			dbscan(dataSet, 0.02, 8);
-=======
-			dataSet = dao.getAllByDayAndHour(days[0], "13:00:00", "14:00:00", classes[0]);
-			dayClusters.put(days[0], dbscan(dataSet, 1.2, 8));
->>>>>>> branch 'master' of https://github.com/gustavoaires/tdrivebeijing.git
+			dbscan(dataSet, 1.2, 8);
 		}
 		dao.close();
 
@@ -52,9 +42,7 @@ public class DBScan {
 	 * @param minPoints
 	 * @return List of Clusters
 	 */
-	private static List<Cluster> dbscan(List<DayDrive> dataSet, Double eps, int minPoints) {
-		List<Cluster> regions = new ArrayList<Cluster>();
-		
+	private static void dbscan(List<DayDrive> dataSet, Double eps, int minPoints) {
 		for (DayDrive point : dataSet) {
 			if (!point.isVisited()) {
 				point.setVisited(true);
@@ -64,37 +52,19 @@ public class DBScan {
 				else {
 					Set<DayDrive> c = new HashSet<DayDrive>();
 					Cluster cluster = new Cluster();
-					cluster.points.addAll(expandCluster(point, neighbors, c, eps, 
-							minPoints, dataSet, cluster.getItsId()));
-					regions.add(cluster);
+					cluster.points.addAll(expandCluster(point, neighbors, c, eps, minPoints, dataSet));
 				}
 			}
 		}
-		
-		return regions;
 	}
 
-	/**
-	 * 
-	 * @param point candidato a core
-	 * @param neighbors estao dentro do eps
-	 * @param cluster novo cluster a ser criado
-	 * @param eps raio de distancia maxima
-	 * @param minPoints quantidade minima de pontos no cluster
-	 * @param dataSet conjunto dos dados
-	 * @param clusterId id do novo cluster criado
-	 * @return novo cluster preenchido
+	/*
+	 * Falta decidir o que vai retornar
 	 */
-<<<<<<< HEAD
 	private static Set<DayDrive> expandCluster(DayDrive point, Set<DayDrive> neighbors, Set<DayDrive> cluster,
 			Double eps, int minPoints, List<DayDrive> dataSet) {
 		
-=======
-	private static Set<DayDrive> expandCluster(DayDrive point, Set<DayDrive> neighbors, Set<DayDrive> cluster, 
-			Double eps,	int minPoints, List<DayDrive> dataSet, int clusterId) {
->>>>>>> branch 'master' of https://github.com/gustavoaires/tdrivebeijing.git
 		cluster.add(point);
-<<<<<<< HEAD
 
 		for (DayDrive p : neighbors) {
 			if (!p.isVisited()) {
@@ -105,46 +75,22 @@ public class DBScan {
 					neighbors.addAll(pNeighbors);
 				}
 					
-=======
-		Set<DayDrive> pNeighbors= new HashSet<DayDrive>();
-		// Precisa rever essa parte do algoritmo
-		// É necessario adicionar mais elementos no conjunto enquanto percorre
-		do {
-			for (DayDrive p : neighbors) {
-				if (!p.isVisited()) {
-					p.setVisited(true);
-					pNeighbors = regionQuery(p, eps, dataSet);
-					if (pNeighbors.size() >= minPoints);
-				}
-				if (p.getCluster() == -1) {
-					cluster.add(p);
-					p.setCluster(clusterId);
-				}
->>>>>>> branch 'master' of https://github.com/gustavoaires/tdrivebeijing.git
 			}
-<<<<<<< HEAD
 
 			if (p.getCluster() == -1)
 				cluster.add(p);
 		}
-=======
-			neighbors.addAll(pNeighbors);
-		} while (false);
-		
->>>>>>> branch 'master' of https://github.com/gustavoaires/tdrivebeijing.git
 		return cluster;
 	}
 
 	private static Set<DayDrive> regionQuery(DayDrive point, Double eps, List<DayDrive> dataSet) {
 		Set<DayDrive> neighbors = new HashSet<DayDrive>();
 		neighbors.add(point);
-		
 		for (DayDrive other : dataSet) {
 			if (euclideanDistance(point, other) <= eps)
 				neighbors.add(other);
 				other.setVisited(true);
 		}
-		
 		return neighbors;
 	}
 
